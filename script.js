5e1e785e7845358aabf3986d834cbe7df27a092d
Kenneth Kim <kkm2059@gmail.com>
2021-10-21 16:32:05 -0400

    Draft 1 - Updated Index.html, Script.js and Style.css

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+let listhoverover = document.getElementById("listhoverover");
+listhoverover.addEventListener("mouseenter", function( event ) {
+    event.target.style.color = "pink";
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 750);
+    }, false);
+    listhoverover.addEventListener("mouseover", function( event ) {
+        event.target.style.color = "red";
+        setTimeout(function() {
+            event.target.style.color = "";
+          }, 750);
+        }, false);
+
+        //
+
+        let listhoverover2 = document.getElementById("listhoverover2");
+
+listhoverover2.addEventListener("mouseenter", function( event ) {
+  event.target.style.color = "purple";
+
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 750);
+}, false);
+
+listhoverover2.addEventListener("mouseover", function( event ) {
+  event.target.style.color = "orange";
+
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 750);
+}, false);
